53e443870949ada62c967f18fce5ef374e227786
Shrung <shrungbhatt@gmail.com>
2017-07-23 21:12:52 +0530

    intial checkin

---
+++ app/src/main/java/com/example/android/visitormanagement/MonumentListActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.visitormanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonumentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monument_list);
+    }
+}
